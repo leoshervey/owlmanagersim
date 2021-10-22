497a14fa829539accc5fbb4451501ed5697bbb9e
Leo Shervey <s20272@crgs.org.uk>
2021-10-22 11:15:41 +0100

    End of Lesson (22/10/21)

--- Match.cs
+++ Match.cs
@@ -8,5 +8,26 @@ namespace OWLSimGame
 {
     class Match
     {
+
+        protected int winLoss(int team1, int team2, int heroStrength1, int heroStrength2)
+        {
+            int weaker, stronger;
+            double winPercent;
+            if (team1 > team2)
+            {
+                weaker = team2;
+                stronger = team1;
+            }
+            else if (team1 < team2)
+            {
+                weaker = team1;
+                stronger = team2;
+            }
+            else
+            {
+                Random rnd = new Random();
+                int 
+            }
+        }
     }
 }
--- WinLossAlgorithm.cs
+++ WinLossAlgorithm.cs
@@ -8,5 +8,6 @@ namespace OWLSimGame
 {
     class WinLossAlgorithm
     {
+
     }
 }
